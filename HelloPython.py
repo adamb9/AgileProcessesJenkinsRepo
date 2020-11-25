9e84d54747597dc0a77cecfd58e3115ac2f8da3a
Aphral <aphral.griffin@mycit.ie>
2020-11-25 13:18:25 +0000

    failing test

--- HelloPython.py
+++ HelloPython.py
@@ -3,4 +3,10 @@ print("Hello world!")
 now = datetime.datetime.now()
 print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
-print("Triggering A Successful Build In Jenkins")		
+def this_fails():
+   x = 1/0
+
+try:
+   this_fails()
+except ZeroDivisionError as err:
+    print('Handling run-time error:', err)
