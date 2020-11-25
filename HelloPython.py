e2dfed12d64b7a056967f6d9b2bf286a882a8290
Adam Baldwin <adam.baldwin@mycit.ie>
2020-11-25 16:19:16 +0000

    please fail

--- HelloPython.py
+++ HelloPython.py
@@ -1,3 +1,5 @@
+import sys
+
 import datetime
 print("Hello world!")
 now = datetime.datetime.now()
@@ -5,6 +7,6 @@ print ("Current date and time is ")
 print (now.strftime("%A, %d-%m-%Y : %H:%M"))
 def this_fails():
    x = 1/0
+this_fails()
 
-
-   this_fails()
+sys.exit(-1)
