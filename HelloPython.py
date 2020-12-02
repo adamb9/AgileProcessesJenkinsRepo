40f469a98ea29beadca2d2550df87a3cdbe854c8
Aphral <aphral.griffin@mycit.ie>
2020-12-02 11:53:41 +0000

    fail maybe

--- HelloPython.py
+++ HelloPython.py
@@ -1,12 +1,16 @@
-import sys
-
 import datetime
-print("Hello world!")
-now = datetime.datetime.now()
-print ("Current date and time is ")
-print (now.strftime("%A, %d-%m-%Y : %H:%M"))
-def this_fails():
-   x = 1/0
-this_fails()
 
-sys.exit(-1)
+
+# print("Hello world!")
+# now = datetime.datetime.now()
+# print("Current date and time is ")
+# print(now.strftime("%A, %d-%m-%Y : %H:%M"))
+def this_fails():
+    rint("hi")
+
+
+def main():
+    this_fails()
+
+
+main()
